511308dcfc2bde35c42b844b247cd87fcd2fdb76
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 13:32:55 -0300

    Criado a Entidade Student

---
+++ src/main/java/com/pac/acentueaqui/models/users/Student.java
@@ -0,0 +1,34 @@
+package com.pac.acentueaqui.models.users;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@SequenceGenerator(name = "student_seq", sequenceName = "student_seq",
+        initialValue = 2, allocationSize = 1)
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq")
+    @Column(name = "id")
+    private Long code;
+
+    @Column(unique=true, length = 20)
+    @NotNull(message = "Matrícula não pode ser nulo")
+    private String registration;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "Data de nascimento não pode ser nulo")
+    private LocalDate birthDate;
+
+    @ManyToOne
+    private User user;
+}
