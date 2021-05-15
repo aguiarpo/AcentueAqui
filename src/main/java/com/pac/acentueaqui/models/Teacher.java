5333eaef1148b04ded0b5bcd34e0d44589cab03d
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 13:02:43 -0300

    Removido imports

--- src/main/java/com/pac/acentueaqui/models/Teacher.java
+++ src/main/java/com/pac/acentueaqui/models/Teacher.java
@@ -1,7 +1,5 @@
 package com.pac.acentueaqui.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
