e654b61c9356488eb8331d77991e5aad282b7d61
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 12:20:58 -0300

    Transformado em Entidade

--- src/main/java/com/pac/acentueaqui/models/User.java
+++ src/main/java/com/pac/acentueaqui/models/User.java
@@ -1,4 +1,30 @@
 package com.pac.acentueaqui.models;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@SequenceGenerator(name = "user_seq", sequenceName = "user_seq",
+        initialValue = 2, allocationSize = 1)
+public class User extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Column(name = "id")
+    private Long code;
+
+    @NotNull(message = "Nome não pode ser Nulo")
+    private String name;
+
+    @NotEmpty(message = "Senha não pode estar vazia")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private LevelsOfAccess levelsOfAccess;
 }
