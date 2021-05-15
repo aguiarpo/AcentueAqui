1f63b91747af8cdda27db1efe1dc2de042c4b148
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 11:57:07 -0300

    Initial commit

---
+++ src/main/java/com/pac/acentueaqui/AcentueAquiApplication.java
@@ -0,0 +1,13 @@
+package com.pac.acentueaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcentueAquiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcentueAquiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pac/acentueaqui/AcentueAquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pac.acentueaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcentueAquiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
