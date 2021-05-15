ea91df682596b82c78ca37b6a7eec0ad01123229
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 16:40:11 -0300

    Adicionado entidade QuestionStudent

---
+++ src/main/java/com/pac/acentueaqui/models/questions/QuestionStudent.java
@@ -0,0 +1,45 @@
+package com.pac.acentueaqui.models.questions;
+
+import com.pac.acentueaqui.models.users.Student;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+@SequenceGenerator(name = "question_student_seq", sequenceName = "question_student_seq",
+        initialValue = 2, allocationSize = 1)
+public class QuestionStudent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_student_seq")
+    @Column(name = "id")
+    private Long code;
+
+    @NotNull(message = "Tempo não pode ser nulo")
+    @Temporal(TemporalType.TIME)
+    @DateTimeFormat(pattern = "hh:mm:ss")
+    private LocalTime publicationTime;
+
+    @NotNull(message = "Tentativas não pode ser nulo")
+    private Integer attempts;
+
+    @NotNull(message = "Resposta certa não pode ser nulo")
+    private Integer rightAnswer;
+
+    @NotNull(message = "XP não pode ser nulo")
+    private Integer xp;
+
+    @ManyToOne
+    @JoinColumn
+    private Question question;
+
+
+    @ManyToOne
+    @JoinColumn
+    private Student student;
+}
