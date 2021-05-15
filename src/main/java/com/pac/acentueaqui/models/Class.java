c5d0b819d9525e37fd8d405c0eedc55342a78d6c
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 13:32:29 -0300

    Criado a Entidade Class

---
+++ src/main/java/com/pac/acentueaqui/models/Class.java
@@ -0,0 +1,29 @@
+package com.pac.acentueaqui.models;
+
+import com.pac.acentueaqui.models.users.School;
+import com.pac.acentueaqui.models.users.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@SequenceGenerator(name = "class_seq", sequenceName = "class_seq",
+        initialValue = 2, allocationSize = 1)
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "class_seq")
+    @Column(name = "id")
+    private Long code;
+
+    @NotNull(message = "Nome não pode ser Nulo")
+    private String name;
+
+    @ManyToOne
+    private School school;
+
+}
