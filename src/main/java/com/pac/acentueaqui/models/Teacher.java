1d016bbf6f67ce9293e0aef0daeef50d69ac4adc
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-15 12:58:58 -0300

    Transformado em Entidade

--- src/main/java/com/pac/acentueaqui/models/Teacher.java
+++ src/main/java/com/pac/acentueaqui/models/Teacher.java
@@ -1,4 +1,28 @@
 package com.pac.acentueaqui.models;
 
-public class Teacher {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@SequenceGenerator(name = "teacher_seq", sequenceName = "teacher_seq",
+        initialValue = 2, allocationSize = 1)
+public class Teacher{
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "teacher_seq")
+    @Column(name = "id")
+    private Long code;
+
+    @Email
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<User> users;
 }
