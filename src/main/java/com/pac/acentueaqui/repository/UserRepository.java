cb37f21ef6ce94a41a46eff3796806004930e660
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-17 20:09:59 -0300

    Criado findByUsername

--- src/main/java/com/pac/acentueaqui/repository/UserRepository.java
+++ src/main/java/com/pac/acentueaqui/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     List<User> findByLevelsOfAccess(LevelsOfAccess levelsOfAccess);
-
+    User findByUsername(String username);
 }
