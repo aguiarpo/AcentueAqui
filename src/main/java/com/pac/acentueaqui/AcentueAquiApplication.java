1b3cbb73c93b6ec82a6df00c6427880465d57a90
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-17 19:01:44 -0300

    Adicionado função started

--- src/main/java/com/pac/acentueaqui/AcentueAquiApplication.java
+++ src/main/java/com/pac/acentueaqui/AcentueAquiApplication.java
@@ -3,8 +3,16 @@ package com.pac.acentueaqui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class AcentueAquiApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AcentueAquiApplication.class, args);
