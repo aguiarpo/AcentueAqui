e04e1f9b1dd43db8e7753dc845ae5c519932f1db
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-17 19:40:13 -0300

    Adicionado findByLevelsOfAccess

--- src/main/java/com/pac/acentueaqui/repository/UserRepository.java
+++ src/main/java/com/pac/acentueaqui/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.pac.acentueaqui.repository;
 
+import com.pac.acentueaqui.models.LevelsOfAccess;
 import com.pac.acentueaqui.models.users.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+    List<User> findByLevelsOfAccess(LevelsOfAccess levelsOfAccess);
 
 }
