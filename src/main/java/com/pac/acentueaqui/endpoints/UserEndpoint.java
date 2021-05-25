b936d8f5929475bf2cd0fef1ecbe9299b361acd3
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-25 20:35:41 -0300

    Adicionado getLogin

--- src/main/java/com/pac/acentueaqui/endpoints/UserEndpoint.java
+++ src/main/java/com/pac/acentueaqui/endpoints/UserEndpoint.java
@@ -1,8 +1,14 @@
 package com.pac.acentueaqui.endpoints;
 
-import com.pac.acentueaqui.repository.AdminRepository;
+import com.pac.acentueaqui.models.users.User;
 import com.pac.acentueaqui.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +21,12 @@ public class UserEndpoint {
     @Autowired
     public UserEndpoint(UserRepository userDao){
         this.userDao = userDao;
+    }
+
+    @GetMapping(path = "/user/get/login")
+    public ResponseEntity<?> getLogin(@AuthenticationPrincipal UserDetails auth){
+        User user = userDao.findByUsername(auth.getUsername());
+        user.getCode();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
