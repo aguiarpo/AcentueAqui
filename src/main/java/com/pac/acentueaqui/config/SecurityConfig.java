977316de8a1c83b8f9627378bc79516afa949650
Eduardo <eduardo.aguiarpo@gmail.com>
2021-05-25 20:34:13 -0300

    Criado SecurityConfig

---
+++ src/main/java/com/pac/acentueaqui/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.pac.acentueaqui.config;
+
+import com.pac.acentueaqui.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.constraints.Null;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // Classe que configura o security do spring
+    private final CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailService customUserDetailService) {
+        this.customUserDetailService = customUserDetailService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/*/user/**").hasRole("USER")
+                .antMatchers("/*/admin/**").hasRole("ADMIN")
+                .antMatchers("/*/vet/**").hasRole("VET")
+                .and()
+                .httpBasic()
+                .and()
+                .cors()
+                .and()
+                .csrf().disable();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
