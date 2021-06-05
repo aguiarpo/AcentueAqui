dcf34d89b1a17125b5ee8c4b2e67ee4da7e07917
Eduardo <eduardo.poerner@catolicasc.org.br>
2021-06-05 17:51:23 -0300

    Adicionado funções update

--- src/main/java/com/pac/acentueaqui/endpoints/AdminEndpoint.java
+++ src/main/java/com/pac/acentueaqui/endpoints/AdminEndpoint.java
@@ -3,28 +3,24 @@ package com.pac.acentueaqui.endpoints;
 import com.pac.acentueaqui.models.LevelsOfAccess;
 import com.pac.acentueaqui.models.users.Admin;
 import com.pac.acentueaqui.models.users.User;
-import com.pac.acentueaqui.repository.AdminRepository;
-import com.pac.acentueaqui.repository.UserRepository;
+import com.pac.acentueaqui.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("v1")
 public class AdminEndpoint {
 
-    private AdminRepository adminDao;
-    private UserRepository userDao;
+    private final AdminRepository adminDao;
+    private final UserRepository userDao;
 
     @Autowired
     public AdminEndpoint(AdminRepository adminDao,
-                         UserRepository userDao){
+                         UserRepository userDao) {
         this.adminDao = adminDao;
         this.userDao = userDao;
     }
@@ -39,16 +35,41 @@ public class AdminEndpoint {
             user.setPassword("admin123");
             user.setBcryptPassword();
             user.setUsername("eduardo.aguiarpo@gmail.com");
-            user.setCreatedBy("Sistema");
-            user.setCreatedDate(LocalDateTime.now());
             Admin admin = new Admin();
             admin.setEmail("eduardo.aguiarpo@gmail.com");
             admin.setUser(user);
             Admin savedAdmin = adminDao.save(admin);
-            if(savedAdmin != null)
-                return new ResponseEntity<>(savedAdmin,HttpStatus.OK);
+            return new ResponseEntity<>(savedAdmin,HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @PutMapping(path = "admin/save")
+    public ResponseEntity<?> updateAdmin(@RequestBody Admin admin){
+        Admin find = adminDao.findByCode(admin.getCode());
+        admin.getUser().setCode(find.getUser().getCode());
+        admin.getUser().setLevelsOfAccess(find.getUser().getLevelsOfAccess());
+        admin.getUser().setUsername(admin.getEmail());
+        admin.getUser().setPassword(find.getUser().getPassword());
+        Admin saved = adminDao.save(admin);
+        return new ResponseEntity<>(saved, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "admin/findall")
+    public ResponseEntity<?> findAll(){
+        List<Admin> admins = adminDao.findAll();
+        return new ResponseEntity<>(admins, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "admin/findbycode/{code}")
+    public ResponseEntity<?> findByCode(@PathVariable Long code){
+        Admin admin = adminDao.findByCode(code);
+        return new ResponseEntity<>(admin, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "admin/findbyemail/{email}")
+    public ResponseEntity<?> findByEmail(@PathVariable String email){
+        Admin admin = adminDao.findByEmail(email);
+        return new ResponseEntity<>(admin, HttpStatus.OK);
+    }
 }
